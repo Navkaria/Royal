5e36ee48785a79daf8ed4c24c2b5e9f04fbbd0ba
Navkaria <107428836+Navkaria@users.noreply.github.com>
2022-08-20 22:54:35 +0300

    upd

---
+++ adsid/integrator.js
@@ -0,0 +1,7 @@
+processGoogleToken({
+   "newToken": "ChAI8IyCmAYQ1fjEvpPttfxREjkAI34A7W65vpDeD0N7zlQnMlklf-b1I1SuYeIF_Ks3fwCTq150bPwqMEggEiXN-Vaxu3MujnIx1g4",
+   "validLifetimeSecs": 300,
+   "freshLifetimeSecs": 300,
+   "1p_jar": "2022-07-23-22",
+   "pucrd": ""
+});
